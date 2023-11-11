ed2b1ca4dac6149a111180f8280768d6e71a8c31
ed55567 <edblade555@gmail.com>
2023-11-11 11:31:08 -0500

    add style and class

--- components/Movie.js
+++ components/Movie.js
@@ -2,17 +2,19 @@
 import styles from '/styles.module.css';
 
 const Movie = ({ title, poster_path, release_date, overview, }) => (
+<div className={styles.movieCardContainer}> 
   <div className={styles.movieCard}>
-    <h2 className={styles.movieTitle}>{title}</h2>
+    {/* <h2 className={styles.movieTitle}>{title}</h2> */}
     <img
       src={`https://image.tmdb.org/t/p/w500${poster_path}`}
       alt={`${title} Poster`}
       className={styles.poster}
     />
-    <p className={styles.movieReleaseDate}>Release Date: {release_date}</p>
+    {/* <p className={styles.movieReleaseDate}>Release Date: {release_date}</p>
     <h3 className={styles.movieSummaryTitle}>Summary:</h3>
-    <p className={styles.movieDescription}>Description: {overview}</p>
+    <p className={styles.movieDescription}>Description: {overview}</p> */}
   </div>
+</div>
 );
 
 export default Movie;
