2064d66c4306521c2a4470a5967cb27851c7808a
ed55567 <edblade555@gmail.com>
2023-11-10 23:58:43 -0500

    updated css file

--- components/Movie.js
+++ components/Movie.js
@@ -1,17 +1,17 @@
 // components/Movie.js
-import styles from '../styles.module.css';
+import styles from '/styles.module.css';
 
 const Movie = ({ title, poster_path, release_date, overview, }) => (
   <div className={styles.movieCard}>
-    <h2>{title}</h2>
+    <h2 className={styles.movieTitle}>{title}</h2>
     <img
       src={`https://image.tmdb.org/t/p/w500${poster_path}`}
       alt={`${title} Poster`}
       className={styles.poster}
     />
-    <p>Release Date: {release_date}</p>
-    <h3>Summary:</h3>
-    <p>Description: {overview}</p>
+    <p className={styles.movieReleaseDate}>Release Date: {release_date}</p>
+    <h3 className={styles.movieSummaryTitle}>Summary:</h3>
+    <p className={styles.movieDescription}>Description: {overview}</p>
   </div>
 );
 
